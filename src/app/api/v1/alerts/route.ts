02ada6250837e455e2fe8ed8b6ebddcc7e04424e
Prashant <104677763+pshdotgg@users.noreply.github.com>
2024-04-25 08:07:41 +0530

    Add alerts api endpoint

---
+++ src/app/api/v1/alerts/route.ts
@@ -0,0 +1,78 @@
+export async function GET() {
+  const data = [
+    {
+      title: 'David Raised Concern',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'concern',
+    },
+    {
+      title: 'Reefer Temp. out of range',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'temp',
+    },
+    {
+      title: 'John Raised Concern',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'concern',
+    },
+    {
+      title: 'Reefer Temp. out of range',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'temp',
+    },
+    {
+      title: 'David Raised Concern',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'concern',
+    },
+    {
+      title: 'Reefer Temp. out of range',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'temp',
+    },
+    {
+      title: 'David Raised Concern',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'concern',
+    },
+    {
+      title: 'Reefer Temp. out of range',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua',
+      date: '13 Feb 24',
+      loadNumber: 12454,
+      customer: 'RoaDo demo Bangalore',
+      type: 'temp',
+    },
+  ]
+
+  return Response.json(data)
+}
